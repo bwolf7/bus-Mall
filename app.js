3d8a12a9b4248a68d641ac471e7a79ecac06c0ad
Brandon <brandon.wolf1@outlook.com>
2021-09-30 09:01:10 -0700

    Added chart to bus amll

--- app.js
+++ app.js
@@ -17,22 +17,23 @@
     
         productObject.countDisplayed = 1 + productObject.countDisplayed;
     }
+    let previouslyDisplayed = [];
 
     function displayNRandomImages() {
-        let alreadyDisplayed = [];
         let randomIndex;
-    
+        let currentlyDisplayed = [];
+
         document.getElementById("image-group").innerHTML = '';
     
         for (let i = 0; i < numberOfImagesToShow; i++) {
             do {
-                randomIndex = Math.floor(Math.random() * Product.productList.length); 
-            } while (alreadyDisplayed.includes(randomIndex));
-           
-            alreadyDisplayed.push(randomIndex);
+                randomIndex = Math.floor(Math.random() * Product.productList.length)
+            } while (currentlyDisplayed.includes(randomIndex) || previouslyDisplayed.includes(randomIndex));
+            
+            currentlyDisplayed.push(randomIndex);
             displayImage(Product.productList[randomIndex]);
         }
-    
+            previouslyDisplayed = currentlyDisplayed;
     }
 
     function logClick(event) {
@@ -75,8 +76,111 @@
             textEl.innerText = `${Product.productList[i].name} was clicked ${Product.productList[i].countClicked} time, and was seen ${Product.productList[i].countDisplayed} times.`
             document.getElementById("image-group").appendChild(textEl);
         }
+        //Code taken from CHARTJS @ https://www.chartjs.org/docs/latest/
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['bag',
+                'banana',
+                'bathroom',
+                'boots',
+                'breakfast',
+                'bubblegum',
+                'chair',
+                'dog-duck',
+                'pen',
+                'pet-sweep',
+                'scissors',
+                'shark',
+                'sweep',
+                'unicorn',
+                'water-can',
+                'wine-glass'],
+                
+                datasets: [{
+                    label: '# of Clicks',
+                    data: [Product.productList[0].countClicked,Product.productList[1].countClicked,Product.productList[2].countClicked,Product.productList[3].countClicked,Product.productList[4].countClicked,Product.productList[5].countClicked,Product.productList[6].countClicked,Product.productList[7].countClicked,Product.productList[8].countClicked,Product.productList[9].countClicked,Product.productList[10].countClicked,Product.productList[11].countClicked,Product.productList[12].countClicked,Product.productList[13].countClicked,Product.productList[14].countClicked,Product.productList[15].countClicked],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });  
+        var ctx = document.getElementById('myChart2').getContext('2d');
+        var myChart2 = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['bag',
+                'banana',
+                'bathroom',
+                'boots',
+                'breakfast',
+                'bubblegum',
+                'chair',
+                'dog-duck',
+                'pen',
+                'pet-sweep',
+                'scissors',
+                'shark',
+                'sweep',
+                'unicorn',
+                'water-can',
+                'wine-glass'],
+                
+                datasets: [{
+                    label: '# of Displays',
+                    data: [Product.productList[0].countDisplayed,Product.productList[1].countDisplayed,Product.productList[2].countDisplayed,Product.productList[3].countDisplayed,Product.productList[4].countDisplayed,Product.productList[5].countDisplayed,Product.productList[6].countDisplayed,Product.productList[7].countDisplayed,Product.productList[8].countDisplayed,Product.productList[9].countDisplayed,Product.productList[10].countDisplayed,Product.productList[11].countDisplayed,Product.productList[12].countDisplayed,Product.productList[13].countDisplayed,Product.productList[14].countDisplayed,Product.productList[15].countDisplayed],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });    
+    }  
     
-    }
     
     const files = [
     'bag.jpg',
